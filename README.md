6e347eaf38a0096c0395455135967278b7aa770f
JJ <jj@jjkeith.net>
2016-06-24 11:18:59 -0700

    Created file structure, some basic code

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Be sure to run the following when you're getting started:
 `npm install --save body-parser bcrypt-nodejs connect-flash cookie-parser ejs express-ejs-layouts express-session morgan passport passport-local express mongoose`
+
+If you add more npm's then please make a note of it here, so the rest of us can add them too. 
---
+++ models/User.js
@@ -0,0 +1,27 @@
+var
+  mongoose = require('mongoose'),
+  bcrypt = require('bcrypt-nodejs'),
+  Schema = mongoose.Schema,
+  userSchema = new Schema({
+    local: {
+      name: String,
+      email: String,
+      password: String
+    }
+  })
+
+// the function run when the user is created.
+// 8 is the number of times the data is encrypted.
+// The more times you encrypt, the more server space you take.
+userSchema.methods.generateHash = function(password){
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
+}
+
+//function used when the user is signing in
+userSchema.methods.validPassword = function(password){
+  return bcrypt.compareSync(password, this.local.password)
+}
+
+var User = mongoose.model('User', userSchema)
+
+module.exports = User
